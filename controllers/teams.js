18f544d94415932da0edbe116ef40cf8fc3bae64
Zack Fiddler <zack.fiddler@me.com>
2022-11-07 19:25:01 -0800

    add visible show page

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,25 +1,31 @@
 const Team = require('../models/team')
-const { move } = require('../routes')
 
 module.exports = {
-index,
-new: newTeam,
-create
-}
-
-function index(req, res) {
-    Team.find({}, function(err, teams) {
-        res.render('teams/index', {title: 'All Teams', teams })
-    })
-}
-
-function newTeam(req,res) {
-    res.render('teams/new', { title : 'Add Team'})
-}
-function create(req,res) {
-    const team = new Team(req.body)
-    team.save(function(err) {
-        if (err) return res.redirect('teams/new')
-        res.redirect('/teams')
-    })
-}
+    index,
+    new: newTeam,
+    create,
+    show
+    }
+    
+    function index(req, res) {
+        Team.find({}, function(err, teams) {
+            res.render('teams/index', {title: 'All Teams', teams })
+        })
+    }
+    
+    function newTeam(req,res) {
+        res.render('teams/new', { title : 'Add Team'})
+    }
+    function create(req,res) {
+        const team = new Team(req.body)
+        team.save(function(err) {
+            if (err) return res.redirect('teams/new')
+            res.redirect('/teams')
+        })
+    }
+    
+    function show(req,res) {
+        Team.findById(req.params.id, function(err, movie) {
+        res.render('teams/show', { title: 'Team Detail', movie})
+        })
+    }
--- routes/teams.js
+++ routes/teams.js
@@ -5,5 +5,6 @@ const teamsCtrl = require('../controllers/teams');
 router.get('/', teamsCtrl.index)
 router.get('/new', teamsCtrl.new)
 router.post('/', teamsCtrl.create)
+router.get('/:id', teamsCtrl.show)
 
 module.exports = router
