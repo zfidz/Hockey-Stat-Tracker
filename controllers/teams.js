b0f3647af5b37709b224d6c38257e3132c105877
Zack Fiddler <zack.fiddler@me.com>
2022-11-07 16:09:06 -0800

    setup some ejs files

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,5 +1,11 @@
 const Team = require('../models/team')
 
 module.exports = {
+index,
+}
 
+function index(req, res) {
+    Team.find({}, function(err, movies) {
+        res.render('teams/index', {title: 'All Teams', teams})
+    })
 }
--- routes/teams.js
+++ routes/teams.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const teamsCtrl = require('../controllers/teams');
 
-
+router.get('/', teamsCtrl.index)
 
 module.exports = router
